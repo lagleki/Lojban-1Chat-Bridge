4749435e45214a56c920781e1d746248bc736be7
lagleki <gleki.is.my.name@gmail.com>
2021-01-31 04:36:34 -0800

    fix

---
+++ dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker kill 1chat
+docker rm 1chat
+
+docker run \
+  -d -it \
+  --name 1chat \
+  --memory 1g \
+  --cpus 1 \
+  -p 9091:3000 \
+  -v $(pwd)/src:/home/app/1chat/src/:Z \
+  -v $(pwd)/data:/home/app/1chat/data/:Z \
+  -v $(pwd)/default-config:/home/app/1chat/default-config/:Z \
+  -v $(pwd)/node_modules:/home/app/1chat/node_modules/:Z \
+  1chat
+docker exec -it 1chat bash
